24132105dd06155cd823fb62951820dc38d09ceb
Soren Mathiasen <smo@tradeshift.com>
2018-01-18 10:36:21 +0100

    remove unused parameters
    This will remove unused command line parameters

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash 
 set -euf -o pipefail
 
+arch=$(uname -m)
+if [[ $arch == x86_64* ]]; then
+    cpu="amd64"
+elif  [[ $arch == arm* ]]; then
+    cpu="arm64"
+fi
+
 HELM_ANNOTATE_VERSION=${HELM_ANNOTATE_VERSION:-"0.2"}
 dest_dir="${HELM_PLUGIN_DIR:-"$(helm home)/plugins/helm-annotate"}"
 file="${dest_dir}/helm-annotate.tar.gz"
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
-url="https://github.com/Tradeshift/helm-annotate/releases/download/v${HELM_ANNOTATE_VERSION}/helm-annotate_${HELM_ANNOTATE_VERSION}_${os}_amd64.tar.gz"
+url="https://github.com/Tradeshift/helm-annotate/releases/download/v${HELM_ANNOTATE_VERSION}/helm-annotate_${HELM_ANNOTATE_VERSION}_${os}_${cpu}.tar.gz"
 
 mkdir -p ${dest_dir}
 
